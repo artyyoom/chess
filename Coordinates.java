a943d4ca00b41079ffb22abe8ea05313696cd318
arttt <149004392+artyyoom@users.noreply.github.com>
2024-10-17 10:17:02 +0300

    Add files via upload

--- Coordinates.java
+++ Coordinates.java
@@ -1,5 +1,7 @@
 package com.chess;
 
+import com.chess.piece.CoordinatesShift;
+
 import java.util.Objects;
 
 public class Coordinates {
@@ -9,6 +11,20 @@ public class Coordinates {
     public Coordinates(File file, Integer rank) {
         this.file = file;
         this.rank = rank;
+    }
+
+    public Coordinates shift(CoordinatesShift shift) {
+        return new Coordinates(File.values()[this.file.ordinal() + shift.fileShift], this.rank + shift.rankShift);
+    }
+
+    public boolean canShift(CoordinatesShift shift) {
+        int f = file.ordinal() + shift.fileShift;
+        int r = rank + shift.rankShift;
+
+        if ((f < 0) || (f > 7)) return false; // 0..7 is good
+        if ((r < 1) || (r > 8)) return false; // 1..8 is good
+
+        return true;
     }
 
     @Override
--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 package com.chess;
 
 
+import com.chess.piece.Piece;
+
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         board.setupDefaultPiecePositions();
 
-        BoardConsoleRenderer renderer = new BoardConsoleRenderer();
-        renderer.render(board);
+//        BoardConsoleRenderer renderer = new BoardConsoleRenderer();
+//        renderer.render(board);
+//
+//        Piece piece = board.getPiece(new Coordinates(File.G, 8));
+//        Set<Coordinates> availableMoveSquares = piece.getAvailableMoveSquares(board);
+//
+//        int a = 123;
 
-        int a = 123;
+        Game game = new Game(board);
+        game.gameLoop();
     }
 }
